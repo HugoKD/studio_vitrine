68b462a1e986a571bad6f467f793237e0f792513
HugoKD <h.cadet99@gmail.com>
2023-05-16 19:43:25 +0200

    On commence à mettre le contenu ..

--- studio_vitrine/urls.py
+++ studio_vitrine/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from .views import contact, index, about, work
+from .views import contact, index, about, rebook, requin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,4 +8,7 @@ urlpatterns = [
     path('contact/', contact, name="contact"),
     path('about/', about, name="contact"),
     path('work/', about, name="contact"),
+    path('work/rebook', rebook, name="contact"),
+    path('work/requin', requin, name="contact"),
+    path('work/moto_helmet', requin, name="contact"),
 ]
--- studio_vitrine/views.py
+++ studio_vitrine/views.py
@@ -9,5 +9,8 @@ def contact(request):
 def about(request):
     return render(request, "studio_vitrine/about.html")
 
-def work(request):
-    return render(request, "studio_vitrine/about.html")
+def requin(request):
+    return render(request, "studio_vitrine/requin.html")
+
+def rebook(request):
+    return render(request, "studio_vitrine/rebook.html")
