31f8cd2fe0b3cc0b3e3bec5b8055bccdf9fd1e02
HugoKD <h.cadet99@gmail.com>
2023-05-17 23:20:56 +0200

    On commence à mettre le contenu ..

--- studio_vitrine/urls.py
+++ studio_vitrine/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from .views import contact, index, about, rebook, requin
+from .views import contact, index, about, rebook, requin, moto, bizarre_city, sphinx, booba_bts,uee, ziak
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,7 +8,12 @@ urlpatterns = [
     path('contact/', contact, name="contact"),
     path('about/', about, name="contact"),
     path('work/', about, name="contact"),
-    path('work/rebook', rebook, name="contact"),
-    path('work/requin', requin, name="contact"),
-    path('work/moto_helmet', requin, name="contact"),
+    path('work/rebook', rebook, name="rebook"),
+    path('work/requin', requin, name="requin"),
+    path('work/moto_helmet', moto, name="moto"),
+    path('work/sphinx', sphinx, name="moto"),
+    path('work/bizarre_city', bizarre_city, name="moto"),
+    path('work/booba_bts', booba_bts, name="moto"),
+    path('work/uee', uee, name="moto"),
+    path('work/ziak_bts', ziak, name="moto"),
 ]
--- studio_vitrine/views.py
+++ studio_vitrine/views.py
@@ -14,3 +14,20 @@ def requin(request):
 
 def rebook(request):
     return render(request, "studio_vitrine/rebook.html")
+
+def moto(request):
+    return render(request, "studio_vitrine/moto.html")
+def bizarre_city(request):
+    return render(request, "studio_vitrine/bizarre_city.html")
+
+def uee(request):
+    return render(request, "studio_vitrine/uee.html")
+
+def sphinx(request):
+    return render(request, "studio_vitrine/sphinx.html")
+
+def ziak(request):
+    return render(request, "studio_vitrine/ziak.html")
+
+def booba_bts(request):
+    return render(request, "studio_vitrine/booba.html")
